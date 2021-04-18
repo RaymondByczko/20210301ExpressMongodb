aac8f8c0727668e8fc258bde0c755d6a6d7495cf
Raymond Byczko <raymondbyczko@att.net>
2021-04-18 11:54:47 +0000

    Able to pcr.

--- controllers/conuser.js
+++ controllers/conuser.js
@@ -5,6 +5,15 @@ async function addUser(req, res) {
 	console.log("addUser:start");
 	let Users = produceUsers();
 	console.log("... req.body.name="+req.body.name);
+	// @todo remove the following before production.
+	console.log("... req.body.password="+req.body.password);
+	let password = req.body.password;
+	// Perorming hash of password.
+	// @todo put this into its own module.
+	let salt = bcryptjs.genSaltSync(12);
+	let hash = bcryptjs.hashSync(password, salt);
+	req.body.password=hash;
+
 	let newUser = new Users(req.body);
 	newUser.save((err, user)=>{
 		if (err) {
