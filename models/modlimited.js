694eec9f57a25ed918ba46bb407e91f1d979c816
Raymond Byczko <raymondbyczko@att.net>
2021-04-23 12:25:59 +0000

    Document modlimited.js

--- models/modlimited.js
+++ models/modlimited.js
@@ -4,6 +4,25 @@
 	a website.  For example, a user can be limited by the
 	number of good login attempts, allowing part
 	functionality of a website.
+
+  This module was inspired by the need to need to demo portal type
+	websites, by giving someone a limited account for evaluation
+	purposes.  Although such a user might register themselves, it 
+	is also possible to hand them a sample account.
+  Such a sample account would be limited so as to decrease security
+	risk.
+
+	Resource limitation comes immediately to mind.  They can only
+	upload a certain number of files for a total accumulated size,
+	for example.  Or they can only login say 5 times.
+
+	Limitation can be a very granular subject.  Outright removing the
+	account is one possibility.  But it requires more manual intervention.
+	This module hopes to address some of that granular aspect.
+
+  To do this, this can be seen as middleware.
+	Middleware functionality that can be attached to one or more
+	routes are possible.  
 */
 const mongoose = require('mongoose');
 const util = require('util');
@@ -80,6 +99,31 @@ const LimitedSchema = new Schema({
 		[Number, Number]
 	}
 });
+
+/***
+ * DOCUMENTATION FOR Schema
+ * 
+ * byGoodLoginAttempts: the number of login attempts that
+ * are allowed to succeed, after which the user will not
+ * have access to the website.  This setting is for limiting
+ * the number of times the user can use the protected areas
+ * of the website.
+ * 
+ * This setting can be mixed with other settings.
+ * 
+ * A value of 0 means that the user has no allowed login attempts
+ * that were successful.  The user may supply correct user/pass,
+ * but a setting of 0 will stop them.
+ * 
+ * Indicating infinite can be done by specifying -1.
+ * 
+ * byTimeOfDay: indicates when one or more pages, or the entire
+ * website in general, can be accessed.  Time outside of these can 
+ * be considered blackout times.
+ * 
+ * Basically an array of TimeRangeSchema can be specified.
+ * @todo Local etc and GMT considerations have to be handled.
+ */
 
 let colName = "limitsCOL";
 const Rooms = mongoose.model('LimitedCol', LimitedSchema, colName);
