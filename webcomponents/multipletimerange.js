3a824e9404cc421dcbf5d36b72a92ce68a50d5f7
Raymond Byczko <raymondbyczko@att.net>
2021-04-28 15:18:37 +0000

    Getting onclick binding working for multiple-timerange

--- webcomponents/multipletimerange.js
+++ webcomponents/multipletimerange.js
@@ -5,8 +5,17 @@ class MultipleTimeRange extends HTMLElement {
 		super();
 		let shadowRoot = this.attachShadow({mode: 'open'});
 		const wrapper = document.createElement('div');
+		const time1 = document.createElement('input');
+		const time2 = document.createElement('input');
+		time1.setAttribute('type', 'time');
+		time2.setAttribute('type', 'time');
+
+		wrapper.appendChild(time1);
+		wrapper.appendChild(time2);
+		this.time1 = time1;
+		this.time2 = time2;
 		wrapper.setAttribute('class', 'wrapper_class');
-		wrapper.textContent = this.textContent;
+		// wrapper.textContent = this.textContent;
 		// wrapper.textContent = "somewrap";
 		const slot = document.createElement('slot');
 		let nodes = slot.assignedNodes();
@@ -17,11 +26,17 @@ class MultipleTimeRange extends HTMLElement {
 		const style = document.createElement('style');
 		style.textContent = `.wrapper_class { border: solid red 2px; width: 10em; height: 5em;
 		}`;
-
+		let id_attr = this.hasAttribute('id')?this.getAttribute('id'):"idisnone";
+		alert('id_attr='+id_attr);
 		shadowRoot.append(style);
 		shadowRoot.append(wrapper);
 		alert('MultipleTimeRange::constructor::end');
 	}
+	get value() {
+		alert('getting value');
+		// alert('this.time1='+this.time1.value);
+		return {timeStart: "11:00:00", timeEnd: "11:22:00"}
+;	}
 }
 
 customElements.define('multiple-timerange', MultipleTimeRange);
