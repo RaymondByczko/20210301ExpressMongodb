6c5b46aeb2683e39eb2aa8885f7942be6ecf50d9
Raymond Byczko <raymondbyczko@att.net>
2021-06-08 18:40:05 -0400

    RByczko;2021-06-08; Improved fetch call.

--- fetch.js
+++ fetch.js
@@ -13,10 +13,9 @@ async function fetchDelete(users_id) {
             },
             body: JSON.stringify(data)
         }
-        let retFetch = await fetch(url, opt);
-        alert("retFetch.response="+retFetch.response);
-        alert("JS(retFetch)="+JSON.stringify(retFetch));
-        return retFetch;
+        let response = await fetch(url, opt);
+        await alertFetchResponse(response);
+        return response;
     } catch (e) {
         alert("fetchDelete caught");
         // @todo return bad status here.
@@ -41,4 +40,26 @@ async function fetchUserAll() {
     } catch (e) {
         alert("fetchUserAll caught");
     }
+}
+/*
+ * Displays the response to fetch  with a bunch of
+ * alerts.  Just an example of what can be done.
+ */
+async function alertFetchResponse(response) {
+    alert("alertFetchResponse:start");
+    alert("response="+response);
+    alert("response.headers="+response.headers);
+    let i = 0;
+    for (const pair of response.headers.entries()){
+        alert("pair0, pair1="+pair[0]+":"+pair[1]);
+        i++;
+        if (i===5) {
+            break;
+        }
+    }
+    alert("response.body="+response.body);
+    let bd = await response.json();
+    alert("response.json()="+JSON.stringify(bd));
+    alert("response.status="+response.status);
+    alert("JS(response)="+JSON.stringify(response));
 }
